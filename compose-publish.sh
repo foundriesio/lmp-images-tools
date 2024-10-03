79c2c84e8fd81954ed0b1ae4c83ea9e070488277
Mike Sul <mike.sul@foundries.io>
2024-10-03 12:00:50 +0200

    dockerfile: Control layers manifest injection
    Add an environment variable to control whether to add the layers
    manifest into an app manifest or not. This allows to produce
    OCI-compliant app manifest if `OCI_COMPLIANT_APP` is set to `"1"`.
    
    Eventually, it will allows us controlling it through factory
    configuration once corresponding changes to source.f.io are made.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- compose-publish.sh
+++ compose-publish.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-composectl publish $@
+
+add_layers_manifest=true
+if [ "${OCI_COMPLIANT_APP}" == "1" ]; then
+	add_layers_manifest=false
+fi
+
+composectl publish --layers-manifest=$add_layers_manifest $@
